f5f9b1aa77d87828e37606999d8eed722ad09a64
Duunz <a6521150025@utch.edu.mx>
2024-06-29 21:41:47 -0700

    Se crea la landing page, se hace que la misma sea la primera página a renderizar y se crea boton de login/logout que de momento solo redirecciona

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} w-full h-full`}>
         <Navbar />
-        <div className="h-[calc(100vh-5rem)] container mx-auto">{children}</div>
+        <div className="w-full">{children}</div>
       </body>
     </html>
   );
